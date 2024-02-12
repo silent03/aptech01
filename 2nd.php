6c461b589b04c6c3339be4de0b2d3a7c97984298
silent03 <innocent.faraz3@gmail.com>
2024-02-12 22:11:48 +0500

    "final"

---
+++ 2nd.php
@@ -0,0 +1,170 @@
+<?php
+
+//Airthmatic Operater
+
+// $a = 5;
+// $b = 3;
+
+// echo $a+$b;
+// echo "<br>";
+// echo $a-$b;
+// echo "<br>";
+// echo $a*$b;
+// echo "<br>";
+// echo $a/$b;
+// echo "<br>";
+// echo $a%$b;
+// echo "<br>";
+//  $a++;
+//  echo "<br>";
+// echo $a;
+// echo "<br>";
+//  $b--;
+//  echo "<br>";
+// echo $b;
+
+
+// Comparision Operater
+
+// $a = "12";
+// $b = 12;
+
+// if($a==$b){
+//     echo "is True";
+// }else{
+//     echo "is False";
+// }
+// echo "<br>";
+
+// $a = "12";
+// $b = 12;
+
+// if($a===$b){
+//     echo "is True";
+// }else{
+//     echo "is False";
+// }
+// echo "<br>";
+
+// $a = 2;
+// $b = 20;
+
+// if($a!=$b){
+//     echo "is True";
+// }else{
+//     echo "is False";
+// }
+// echo "<br>";
+// $a = 18;
+// $b = 25;
+// if($a<$b){
+//     echo "Eligible";
+// }else{
+//     echo "Not Eligible";
+// }
+
+
+// echo "<br>";
+// $a = 12;
+// $b = 18;
+// if($a>$b){
+//     echo "voter Eligible";
+// }else{
+//     echo "voter Not Eligible";
+// }
+
+
+// Logical Operater
+
+// $a = "Admin";
+// $b = 1234;
+
+// if($a=="Admin"&&$b==1234){
+//     echo "Admin is login";
+// }else{
+//     echo "Plese check user name and password";
+// }
+
+// echo "<br>";
+
+
+// $a = "Admin";
+// $b = 123;
+
+// if($a=="Admin"||$b==1234){
+//     echo "Admin is login";
+// }else{
+//     echo "Plese check user name and password";
+// // }
+
+// $a=10;
+// $b=10;
+
+// if($a^$b){
+//     echo "yes";
+// }else{
+//     echo "No";
+// }
+ 
+
+// $a=0;
+
+
+// if(!$a){
+//     echo "True";
+// }else{
+//     echo "False";
+// }
+
+
+// Assignment Operater
+
+
+// $a = 5;
+// $b = 5;
+// $c = $a+$b;
+// echo $c;
+
+
+// $a = 5;
+// $b = 10;
+// $b +=$a;
+// echo $b;
+
+
+
+// $a = 15;
+// $b = 20;
+// $b -=$a;
+// echo $b;
+
+
+// $a = 15;
+// $b = 20;
+// $b /=$a;
+// echo $b;
+
+
+
+// $a = 5;
+// $b = 2;
+// $b *=$a;
+// echo $b;
+
+
+$a = 23;
+$b = 2;
+$c = $a%$b;
+echo $c;
+
+echo "<br>";
+
+$a = 23;
+$b = 2;
+$a%=$b;
+echo $a;
+
+
+// echo $a%$b;
+// echo "<br>";
+?>
