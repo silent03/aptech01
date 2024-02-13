f2a727c6f1a3840766cee6226f564c06ce645cc6
silent03 <innocent.faraz3@gmail.com>
2024-02-13 10:55:18 +0500

    "First"

---
+++ 4th.php
@@ -0,0 +1,37 @@
+<?php
+
+
+// While ///
+$n = 2;
+
+while($n <= 15)
+{
+   echo "The Number is : $n <br/>";
+   $n++;
+}
+
+// Do While
+$n = 22;
+ do{
+    echo "The number is : $n <br/>";
+     $n++;
+}while($n<=24);
+
+
+// For
+
+for($n=1;$n<=18;$n++)
+{
+    echo "The number is : $n <br/>";
+}
+
+
+// For each 
+
+$color = array("yellow","red","grey","black");
+foreach($color as $cl)
+{
+    echo "$cl <br/>";
+}
+
+?>
