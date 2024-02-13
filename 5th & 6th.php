6cfa0e8c45797d5327a268bd8ecf9b6a5670bc90
silent03 <innocent.faraz3@gmail.com>
2024-02-13 10:59:18 +0500

    last Commit

---
+++ 5th & 6th.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+<form action="welcome.php" method="POST">
+
+    <div class="container">
+        <h1>Super Global Variable</h1>
+    <div class="form-group">
+      <label for="">Name:</label>
+      <input type="text" class="form-control" name="name" placeholder="Enter Your Name">
+ </div>
+
+ <div class="form-group">
+      <label for="">Password:</label>
+      <input type="password" class="form-control" name="pass" placeholder="Enter Your Password">
+ </div>
+
+
+ <input type="submit" value="Submit" class ="btn btn-dark">
+ </div>
+    </form>
+
+  
+  </body>
+</html>
+
